fc28db76f0b457ecad533252825404da5cbf8403
KENN <cent.rngttwt@gmail.com>
2019-03-26 22:47:52 +0900

    add using tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,37 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+enum {
+    TK_NUM = 256,
+    TK_EOF,
+};
+
+typedef struct {
+    int ty;
+    int val;
+    char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+    int i = 0;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+
+        fprintf(stderr, "can't tokenize %s\n", p);
+        exit(1);
+    }
+
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
+}
+
+void error(int i) {
+    fprintf(stderr, "unexpected token %s\n", tokens[i].input);
+    exit(1);
+}
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "wrong number of argument\n");
-    return 1;
-  }
+    if (argc != 2) {
+        fprintf(stderr, "wrong number of argument\n");
+        return 1;
+}
 
-  char *p = argv[1];
+tokenize(argv[1]);
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global _main\n");
-  printf("_main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+printf(".intel_syntax noprefix\n");
+printf(".global _main\n");
+printf("_main:\n");
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
+if (tokens[0].ty != TK_NUM)
+    error(0);
+printf("  mov rax, %d\n", tokens[0].val);
+
+int i = 1;
+while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+        i++;
+        if (tokens[i].ty != TK_NUM)
+            error(i);
+        printf("  add rax, %d\n", tokens[i].val);
+        i++;
+        continue;
     }
 
-    else if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
+    else if (tokens[i].ty == '-') {
+        i++;
+        if (tokens[i].ty != TK_NUM)
+            error(i);
+        printf("  sub rax, %d\n", tokens[i].val);
+        i++;
+        continue;
     }
-    else {
-      fprintf(stderr, "unexpected char: '%c'\n", *p);
-      return 1;
+    else
+        error(i);
     }
-  }
 
-  printf("  ret\n");
-  return 0;
+    printf("  ret\n");
+    return 0;
 }
