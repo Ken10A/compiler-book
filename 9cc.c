a71eb01bab796f95f7da3118b905b32237010cf4
KENN <cent.rngttwt@gmail.com>
2019-03-28 21:36:13 +0900

    add functions returning new AST nodes

--- 9cc.c
+++ 9cc.c
@@ -14,6 +14,21 @@ typedef struct Node {
     int val;
 } Node;
 
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ty;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = malloc(sizeof(Node));
+    node->ty = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 enum {
     TK_NUM = 256,
     TK_EOF,
