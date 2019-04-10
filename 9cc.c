0d7ac4c75e36b597433b7e50bcb85ca9525bf7a1
KENN <cent.rngttwt@gmail.com>
2019-04-10 22:26:54 +0900

    add improving the code generator

--- 9cc.c
+++ 9cc.c
@@ -134,9 +134,43 @@ Node *term() {
     error("Found a token that is nethrer a number nor an open bracket", tokens[pos].input);
 }
 
+void gen_lval(Node *node) {
+    if (node->ty != ND_IDENT)
+        error("The left term is not variable", "");
+
+    int offset = ('z' - node->name + 1) * 8;
+    printf("    mov rax, rbp\n");
+    printf("    sub rax, %d\n");
+    printf("    push rax\n");
+}
+
 void gen(Node *node) {
     if (node->ty == ND_NUM) {
         printf("    push %d\n", node->val);
+        return;
+    }
+
+    if (node->ty == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    if (node->ty == ND_IDENT) {
+        gen_lval(node);
+        printf("    pop rax\n");
+        printf("    mov rax, [rax]\n");
+        printf("    push rax\n");
+        return;
+    }
+
+    if (node->ty == '=') {
+        gen_lval(node->lhs);
+        gen(node->rhs);
+
+        printf("    pop rdi\n");
+        printf("    pop rax\n");
+        printf("    mov [rax], rdi\n");
+        printf("    push rdi\n");
         return;
     }
 
@@ -236,7 +270,7 @@ Node *assign() {
 Node *stmt() {
     Node *node = assign(){
     if (!consume(';'))
-        error("Found not ';' token: %s", tokens[pos].input);
+        error("Found not ';' token %s", tokens[pos].input);
     }
 }
 
