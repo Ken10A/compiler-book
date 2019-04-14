820b949179fc9d06edc2e4b410a4839e560e73a1
KENN <cent.rngttwt@gmail.com>
2019-04-14 23:25:44 +0900

    add  map

--- 9cc.c
+++ 9cc.c
@@ -304,14 +304,22 @@ void *map_get(Map *map, char *key) {
     return NULL;
 }
 
-int expect(int line, int expected, int actual) {
-    if (expected == actual)
-        return 0;
-    fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
-    exit(1);
+
+void test_map() {
+    Map *map = new_map();
+    expect(__LINE__, 0, (int)map_get(map, "foo"));
+
+    map_put(map, "foo", (void *)2);
+    expect(__LINE__, 2, (int)map_get(map, "foo"));
+
+    map_put(map, "bar", (void *)4);
+    expect(__LINE__, 4, (int)map_get(map, "bar"));
+
+    map_put(map, "foo", (void *)6);
+    expect(__LINE__, 6, (int)map_get(map, "foo"));
 }
 
-void runtest() {
+void test_vector() {
     Vector *vec = new_vector();
     expect(__LINE__, 0, vec->len);
 
@@ -322,7 +330,18 @@ void runtest() {
     expect(__LINE__, 0, (int)vec->data[0]);
     expect(__LINE__, 50, (int)vec->data[50]);
     expect(__LINE__, 99, (int)vec->data[99]);
+}
 
+int expect(int line, int expected, int actual) {
+    if (expected == actual)
+        return 0;
+    fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+    exit(1);
+}
+
+void runtest() {
+    test_vector();
+    test_map();
     printf("OK\n");
 }
 
